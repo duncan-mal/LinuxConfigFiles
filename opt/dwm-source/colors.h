e7dc95c93acf8c8f00d7bad083dd1354c8884b59
Duncan Maltman <140368237+duncan-mal@users.noreply.github.com>
2024-01-13 18:01:52 -0700

    Added colors.h

---
+++ opt/dwm-source/colors.h
@@ -0,0 +1,18 @@
+static const char black[]       = "#2a1e1e";
+static const char gray2[]       = "#2e323a"; // unfocused window border
+static const char gray3[]       = "#545862";
+static const char gray4[]       = "#6d8dad";
+static const char blue[]        = "#61AFEF"; // focused window border
+static const char green[]       = "#98C379";
+static const char red[]         = "#E06C75";
+static const char orange[]      = "#E49263";
+static const char yellow[]      = "#E5C07B";
+static const char pink[]        = "#E98CA4";
+static const char purple[]      = "#C678DD";
+static const char cyan[]        = "#56B6C2";
+static const char col_borderbar[]  = "#2a1e1e"; // inner border
+
+//Custom Colors
+static const char custred1[]    = "#a73334";
+static const char custred2[]    = "#94372a";
+static const char custgray1[]   = "#545862";
